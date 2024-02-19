16f96dafcc3e57631fe13ec6ba5d006d03a7ab97
Xuanhe Zhang <82539791+xuanhe95@users.noreply.github.com>
2024-02-18 20:45:14 -0800

    create project

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+      你好，世界
+    </Button>
+  );
+}
+
+export default App;
