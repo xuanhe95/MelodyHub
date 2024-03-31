2d08874a6aa353e7e9ef8ed4fc90c347de4830b1
Yujia Ye <ye28888@gmail.com>
2024-03-31 12:36:15 -0500

    db test connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,11 +12,13 @@ app.use(express.json());
 app.use("/api", router);
 
 // test query to verify db connection before starting app.
-db.query('SELECT *', (error) => {
+db.query('SELECT * FROM TEST', (error, results) => {
     if (error) {
         console.error('Error connecting to the database:', error);
         process.exit(1); // Exit the app if the database connection fails
     }
+    console.log("db is connected successfully.");
+    console.log(results);
 
     // Only start the Express server after the database connection is established
     app.listen(config.server_port, () => {
